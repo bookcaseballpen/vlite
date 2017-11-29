dd16e82975aee8016e12ed6cbcf8821083ddbca4
Sam Wang <sam_wang@creaf.com>
2017-11-29 17:12:05 +0800

    add $el to vlite

--- main.js
+++ main.js
@@ -1,7 +1,2 @@
-$(document).ready(function() {
-  vlite = new Vlite()
-  vlite.init('#app', './template/test.tpl.html')
-
-  // vlite1 = new Vlite()
-  // vlite1.init('#app1', './template/test.tpl.html')
-})
+vlite = new Vlite()
+vlite.init('#app', '/templates/test.tpl.html')
--- vlite.js
+++ vlite.js
@@ -1,41 +1,52 @@
-var Vlite = function () {
-  var vlite = {
-    init: function (node, tpl) {
-      var _this = this
-      var request = new XMLHttpRequest();
-      request.open('GET', './templates/test.tpl.html', true)
-      request.onload = function () {
-        if (request.status >= 200 && request.status < 400) {
-          //success
-          var data = request.responseText
-          var ele = data.split('<template>')[1].split('</template>')[0]
-          var ctrl = data.split('<script>')[1].split('</script>')[0]
-          var sty = data.split('<style>')[1].split('</style>')[0]
-          _this.create(node, ele, ctrl, sty)
-        } else {
-          //fail
-          console.error('server return error')
+(function (window) {
+  window.Vlite = function () {
+    var vlite = {
+      config: {},
+      el: {},
+      init: function (node, tpl) {
+        var _this = this
+        _this.config.node = node
+        _this.config.tpl = tpl
+        var request = new XMLHttpRequest();
+        if (!node) {
+          console.log('Node is not proivded')
+        }
+        this.el = document.querySelector(node)
+        request.open('GET', tpl, true)
+        request.onload = function () {
+          if (request.status >= 200 && request.status < 400) {
+            //success
+            var data = request.responseText
+            var ele = data.split('<template>')[1].split('</template>')[0]
+            var ctrl = data.split('<script>')[1].split('</script>')[0]
+            var sty = data.split('<style>')[1].split('</style>')[0]
+            _this.create(node, ele, ctrl, sty)
+          } else {
+            //fail
+            console.error('server return error')
+          }
+        }
+        request.onerror = function () {
+          //cannot connect to server
+          console.error('can not connect to server')
+        }
+        request.send()
+      },
+      create: function (node, ele, ctrl, sty) {
+        if (!ele) {
+          console.error('no element is provided in template')
+          return
+        }
+        console.log(this)
+        this.el.innerHTML = ele
+        if (sty) {
+          document.querySelector('head').innerHTML += "<style>" + sty + "</style>"
+        }
+        if (ctrl) {
+          eval(ctrl.replace('$el', "$(this.el)"))
         }
       }
-      request.onerror = function () {
-        //cannot connect to server
-        console.error('can not connect to server')
-      }
-      request.send()
-    },
-    create: function (node, ele, ctrl, sty) {
-      if (!ele) {
-        console.error('no element is provided in template')
-        return
-      }
-      document.querySelector(node).innerHTML += ele
-      if (sty) {
-        document.querySelector('head').innerHTML += "<style>" + sty + "</style>"
-      }
-      if (ctrl) {
-        document.querySelector('body').innerHTML += "<script>" + ctrl + "</script>"
-      }
     }
+    return vlite
   }
-  return vlite
-}
+})(window)
